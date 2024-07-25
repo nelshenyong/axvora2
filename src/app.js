0237beca73bed5c11c3e0b45784d2ee3060097ad
nelshenyong <112174662+nelshenyong@users.noreply.github.com>
2024-07-25 20:12:31 +0700

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,106 @@
+document.addEventListener("alpine:init", () => {
+  Alpine.data("products", () => ({
+    items: [
+      { id: 1, name: "Monarch", img: "1.png", price: 30000 },
+      { id: 2, name: "Radiant", img: "2.png", price: 60000 },
+      { id: 3, name: "Guardian", img: "3.png", price: 90000 },
+      { id: 4, name: "Astra", img: "4.png", price: 120000 },
+      { id: 5, name: "Overlord", img: "5.png", price: 150000 },
+    ],
+  }));
+
+  Alpine.store("cart", {
+    items: [],
+    total: 0,
+    quantity: 0,
+    add(newItem) {
+      // Cek apakah ada barang yang sama di cart
+      const cartItem = this.items.find((item) => item.id === newItem.id);
+
+      // Jika belum ada / kosong
+      if (!cartItem) {
+        this.items.push({ ...newItem, quantity: 1, total: newItem.price });
+        this.quantity++;
+        this.total += newItem.price;
+      } else {
+        // Jika barang sudah ada, cek apakah barang beda atau sama
+        this.items = this.items.map((item) => {
+          // Jika barang beda
+          if (item.id !== newItem.id) {
+            return item;
+          } else {
+            // Jiika barang sudah ada, tambah qty ddan total
+            item.quantity++;
+            item.total = item.price * item.quantity;
+            this.quantity++;
+            this.total += item.price;
+            return item;
+          }
+        });
+      }
+    },
+    remove(id) {
+      // Ambil item yang mau diremove berdasarkan id nya
+      const cartItem = this.items.find((item) => item.id === id);
+
+      // Jika item lebih dari 1
+      if (cartItem.quantity > 1) {
+        // Telusuri satu persatu
+        this.items = this.items.map((item) => {
+          // Jika bukan barang yang bukan di klik
+          if (item.id !== id) {
+            return item;
+          } else {
+            item.quantity--;
+            item.total = item.price * item.quantity;
+            this.quantity--;
+            this.total -= item.price;
+            return item;
+          }
+        });
+      } else if (cartItem.quantity === 1) {
+        // Jika barangnya sisa 1
+        this.items = this.items.filter((item) => item.id !== id);
+        this.quantity--;
+        this.total -= cartItem.price;
+      }
+    },
+  });
+});
+
+// form validation
+const checkoutButton = document.querySelector(".checkout-button");
+checkoutButton.disabled = true;
+
+const form = document.querySelector("#checkoutForm");
+
+form.addEventListener("keyup", function () {
+  for (let i = 0; i < form.elements.length; i++) {
+    if (form.elements[i].value.length !== 0) {
+      checkoutButton.classList.remove("disabled");
+      checkoutButton.classList.add("disabled");
+    } else {
+      return false;
+    }
+  }
+  checkoutButton.disabled = false;
+  checkoutButton.classList.remove("disabled");
+});
+
+// Kirim data ketika checkout button diklik
+checkoutButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const data = new URLSearchParams(formData);
+  const objData = Object.fromEntries(data);
+  console.log(objData);
+});
+
+// Konversi ke Rupiah
+const rupiah = (number) => {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    // minimumFractionDigits: 0,
+  }).format(number);
+};
